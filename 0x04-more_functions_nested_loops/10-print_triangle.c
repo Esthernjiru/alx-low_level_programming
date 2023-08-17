a3bc726642ce04ee08d599f424c5c903b3de5942
Esther Njiru <esthernjiru86@gmail.com>
2023-08-17 15:22:13 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print a trianglr followed by new line code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+	if triangle <=0
+{
+		_putchar('\n');
+	}
+for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+
+}
+
