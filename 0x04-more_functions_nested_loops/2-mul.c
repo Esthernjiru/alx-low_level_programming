c771d2f8b64549fdec860144e3b3f25b84838ebc
Esther Njiru <esthernjiru86@gmail.com>
2023-08-17 14:15:02 -0700

    multiply

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
  * mul - multiply 2 integers where
- * @a- first int
- * @b- second int
+ * @a: first int
+ * @b: second int
  * Return: addition of the multiplication
  */
 
